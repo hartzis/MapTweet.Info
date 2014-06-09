2588f725c53a855a92d3f64a38a75e5fab8eff5b
Brian Emil Hartz <brianhartz@gmail.com>
2014-06-09 17:46:29 -0600

    update about with github, add example-devConf

---
+++ config/example-devConf.js
@@ -0,0 +1,9 @@
+module.exports = {
+  secret: 'yourSecret',
+  googleApiKey: 'yourAPIkey',
+  twitter: {
+    ApiKey: 'yourAPIkey',
+    ApiSecret: 'yourAPIkey',
+    callbackUrl: 'yourCALLBACKURL - /auth/twitter/callback'
+  }
+}
