70e87e6f293f4a0aeeb2bc542a80d5f30bf55cdc
Brian Emil Hartz <brianhartz@gmail.com>
2014-09-29 22:12:38 -0600

    add gulp templates and clean templates

--- Gulpfile.js
+++ Gulpfile.js
@@ -9,28 +9,30 @@ var paths = {
   angular: ['public/js/src/app.js', 'public/js/src/**/*.js'],
   minifiedLocation: 'public/js/min',
   angularAppViews: {
-    jade: './views/app/**/*.jade',
-    hmtl: './public/html'
+    jade: './views/**/*.jade',
+    html: './public/html'
   }
 };
 
-// compile jade templates to public/html
-gulp.task('templates', function() {
-
-  gulp.src('./views/app/**/*.jade')
-    .pipe(jade({
-      locals: angularAppViews.jade
-    }))
-    .pipe(gulp.dest(angularAppViews.html))
-});
-
 // clean old version of minified/uglified app
-gulp.task('clean', function (cb) {
+gulp.task('clean-angularApp', function (cb) {
   del([paths.minifiedLocation+'/*.*'], cb);
 })
 
+// clean old version of minified/uglified app
+gulp.task('clean-templates', function (cb) {
+  del([paths.angularAppViews.html+'/**/*.*'], cb);
+})
+
+// compile jade templates to public/html
+gulp.task('templates', ['clean-templates'], function() {
+  return gulp.src(paths.angularAppViews.jade)
+    .pipe(jade())
+    .pipe(gulp.dest(paths.angularAppViews.html))
+})
+
 // setup uglify of angular app
-gulp.task('angularApp', ['clean'], function () {
+gulp.task('angularApp', ['clean-angularApp'], function () {
     return gulp.src(paths.angular)
     .pipe(uglify('app.min.js', {
       outSourceMap: true
@@ -39,10 +41,13 @@ gulp.task('angularApp', ['clean'], function () {
 })
 
 // watch task setup
-gulp.task('watch', function () {
+gulp.task('start-watch', function () {
   gulp.watch(paths.angular, ['angularApp']);
 })
 
+// watch procedure
+gulp.task('watch', ['start-watch', 'angularApp'])
+
 // default task with no watch
-gulp.task('default', ['angularApp']);
+gulp.task('default', ['angularApp'])
 
